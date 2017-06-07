3961b9770e5a400c839f6573dbc5859119196bdf
Zekai Dis <Zekai.Dis@postkodlotteriet.se>
2017-06-07 17:36:43 +0200

    Removed unneeded streams

--- src/test/java/com/thufir/repository/HashUrlRepositoryTest.java
+++ src/test/java/com/thufir/repository/HashUrlRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.thufir.repository;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.thufir.UrlshortenerApplication;
 import com.thufir.entity.HashUrl;
@@ -13,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+import static com.thufir.utils.TestUtils.getSampleUrls;
 import static com.thufir.utils.TestUtils.hasher;
 import static org.junit.Assert.assertEquals;
 
@@ -29,14 +29,12 @@ public class HashUrlRepositoryTest {
     }
 
     @Test
-    public void saveABook() throws Exception {
+    public void save() throws Exception {
         assertEquals(0, repo.findAll().size());
 
-        ImmutableList<String> healthyUrls = ImmutableList.of("https://www.google.com", "https://www.yahoo.com",
-            "http://www.bbc.co.uk/science/space/solarsystem/sun_and_planets/earth", "http://berserk.wikia.com/wiki/Berserk_(2016_Anime)");
-        ImmutableMap<String, String> urlToHashMap = hashUrlMapBuilder(healthyUrls);
+        ImmutableMap<String, String> urlToHashMap = hashUrlMapBuilder(getSampleUrls());
 
-        healthyUrls.forEach(url -> {
+        getSampleUrls().forEach(url -> {
             HashUrl hashUrl = new HashUrl(urlToHashMap.get(url), url);
             HashUrl saved = repo.save(hashUrl);
 
